f39585b5a00372e55230a1fe3e1cc18ee792ca33
vitor.silva <vitoro580@gmail.com>
2025-02-18 11:07:23 -0400

    feat: add shadcn components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from   "@/components/ui/input";
+
+const ProductPage = () => {
+    return ( 
+        <div>
+            <h1>Page Products</h1> 
+            <Button>Teste</Button>
+            <Input placeholder="Placeholder teste...."></Input>
+        </div>
+    );
+}
+ 
+export default ProductPage;
